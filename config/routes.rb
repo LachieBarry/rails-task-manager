25a35d7250f6410e1f59c9f06a7edb29cd87f9f2
Lachlan Barry <lachlan.j.barry@gmail.com>
2023-11-13 16:50:33 +1100

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -7,25 +7,4 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-
-  Rails.application.routes.draw do
-    # Read all
-    get    'tasks', to: 'tasks#index'
-    # Create
-
-    get    'tasks/new', to: 'tasks#new', as: :new_task
-    post   'tasks', to: 'tasks#create'
-
-    # Read one - The `show` route needs to be *after* `new` route.
-    get    'tasks/:id', to: 'tasks#show', as: :task
-
-    # Update
-    get    'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-    patch  'tasks/:id', to: 'tasks#update'
-
-    # Delete
-    delete 'tasks/:id', to: 'tasks#destroy'
-
-  end
 end
-
